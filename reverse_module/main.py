47f077de6897fee38b827edb5f143f359cf88c00
francesco <francesco.espositoy@gmail.com>
2021-08-02 15:48:05 +0200

    Bugfix

--- reverse_module/main.py
+++ reverse_module/main.py
@@ -15,18 +15,20 @@ def validateIP(ip: str) -> bool:
         return False
 
 def usage() -> str:
-    return f"usage: {sys.argv[0]} [-a | --auto]:\n" \
-           f"trying to sniff a LLDP or CDP packet and waits until\n" \
+    return f"usage: {sys.argv[0]} [-a | --auto] [-i | --interface]:\n" \
+           f"trying to sniff a LLDP or CDP packet on selected interface and waits until\n" \
            f"it receives the packet\n\n" \
+           f"Example: {sys.argv[0]} -a -i eth0" \
            f"{sys.argv[0]} [-m | --manual] IP:\n" \
+           f"Example {sys.argv[0]} -m 10.0.0.1" \
            f"connect directly to the specified IP address without sniffing anything\n"
 
 
 if __name__ == "__main__":
-    if len(sys.argv) == 2 and (sys.argv[1] == "-a" or sys.argv[1] == "--auto"):
+    if len(sys.argv) == 4 and ((sys.argv[1] == "-a" or sys.argv[1] == "--auto") and (sys.argv[2] == "-i" or sys.argv[2] == "--interface")):
         inspector = Inspector()
-        inspector.sniff("eth0")
-    elif len(sys.argv) == 3 and (sys.argv[1] == "-m" or sys.argv[1] == "--manual"):
+        inspector.sniff(sys.argv[3])
+    elif len(sys.argv) == 5 and (sys.argv[1] == "-m" or sys.argv[1] == "--manual"):
         if validateIP(sys.argv[2]):
             inspector = Inspector()
             inspector.manualConnection(sys.argv[2])
