db69990e2a7f687cf395078053c71c5b50ddfb3e
francesco <francesco.espositoy@gmail.com>
2021-08-05 18:43:02 +0200

    Bugfix

--- temp/main.py
+++ temp/main.py
@@ -3,7 +3,7 @@ from Naspy import Naspy
 if __name__ == "__main__":
 
     naspy = Naspy()
-    naspy.sniff("eth0")
+    await naspy.sniff("eth0")
 
     for element in naspy.manager.visited:
         print(element.hostname)
