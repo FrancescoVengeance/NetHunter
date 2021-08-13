0f290d46cc0ca0d8d179948f81e61b52392eb51e
francesco <francesco.espositoy@gmail.com>
2021-08-13 19:45:47 +0200

    Bugfix on buildJson()

--- naspy/Naspy.py
+++ naspy/Naspy.py
@@ -141,7 +141,8 @@ class Naspy:
                 if line[0] == '-':
                     newElements.append(line[1:end]+', "new":"false"}')
 
-        print(newElements)
+        for line in newElements:
+            print(line)
         toRemove = []
         for i in range(len(newElements)):
             je1 = json.loads(newElements[i])
