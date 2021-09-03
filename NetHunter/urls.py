65bf326dcbe3696427319be9ea6f029af60e8dc4
francesco <francesco.espositoy@gmail.com>
2021-09-03 17:29:10 +0200

    Added run method to run topology from webview

--- NetHunter/urls.py
+++ NetHunter/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from pages.views import index_view
+from pages.views import run
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index_view, name="index")
+    path('', index_view, name="index"),
+    path('', run, name="run")
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from naspy import Naspy
 
 
 def index_view(request, *args, **kwargs):
     context = {}
     return render(request, "index.html", context)
+
+
+def run(request, *args, **kwargs):
+    if request.method == "POST":
+        naspy = Naspy()
+        naspy.sniff("enp0s8")
