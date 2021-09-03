f95cd468bc55b4f2bf9abaf9982ffd270c467d6e
francesco <francesco.espositoy@gmail.com>
2021-09-03 18:15:19 +0200

    Bugfix

--- NetHunter/urls.py
+++ NetHunter/urls.py
@@ -16,10 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from pages.views import index_view
-from pages.views import run
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index_view, name="index"),
-    path('', run, name="run")
+    # path('', run, name="run")
 ]
--- pages/views.py
+++ pages/views.py
@@ -4,12 +4,10 @@ from naspy.Naspy import Naspy
 
 def index_view(request, *args, **kwargs):
     context = {}
-    return render(request, "index.html", context)
-
-
-def run(request, *args, **kwargs):
-    print(f"metodo chiamato {request.method}")
-    if request.method == "POST":
+    if request.method == "GET":
+        return render(request, "index.html", context)
+    elif request.method == "POST":
         naspy = Naspy()
         print("Start sniffing")
         naspy.sniff("enp0s3")
+        return render(request, "index.html", context)
