c3ea17b98d285adea646dca1f29a924e7515339e
francesco <francesco.espositoy@gmail.com>
2021-09-07 20:38:37 +0200

    Refactoring
    Started to refactor nethound_module

---
+++ nethound_module/main.py
@@ -0,0 +1,35 @@
+import concurrent.futures
+from NetInterface import *
+from Monitors import *
+from datetime import datetime
+import sys
+import pyshark
+
+
+def print_help():
+    return "Usage: -i [interface], [-m [mode]], [-p [password]], [-h [help]]\n" \
+           "mode options: \n" \
+           "arp: IDS system for ARP protocol." \
+           "dhcp: IDS system for Rogue DHCP Attack" \
+           "dns: IDS system for DNS Hijack Attack" \
+           "stp: Monitoring STP Status and eventually failure" \
+           "default: When no other options are chosen this script will perform all modality\n" \
+           "password: is the password use for decrypting switch credentials"
+
+
+password = None
+
+if __name__ == "__main__":
+    if os.geteuid() != 0:
+        print("You need to run as a root")
+        print(print_help())
+        sys.exit(0)
+
+    if len(sys.argv) < 3:
+        print("[ERROR] you must enter an interface and a modality")
+        print(print_help())
+        sys.exit(0)
+
+    if "-i" in sys.argv:
+        interface = sys.argv[sys.argv.index("-i") + 1]
+        print(f"interface {interface}")
