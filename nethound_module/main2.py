e2a1bd390c18588cdeb88615559e3aacfa73b0d1
francesco <francesco.espositoy@gmail.com>
2021-09-16 20:39:06 +0200

    Added main2 to starting tests

--- nethound_module/main2.py
+++ nethound_module/main2.py
@@ -1,5 +1,9 @@
 from monitors.dhcp_monitor import DHCPMonitor
 from safe_print import SafePrint
 from packets_queue import PacketsQueue
+import sys
 
-dhcp = DHCPMonitor()
+interface = sys.argv[1]
+packets = PacketsQueue()
+safe_print = SafePrint()
+dhcp = DHCPMonitor(interface, packets, safe_print)
