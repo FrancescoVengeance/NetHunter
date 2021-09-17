5fa2ec03f1ed80ca35024fad97d929fbda354537
francesco <francesco.espositoy@gmail.com>
2021-09-17 17:26:26 +0200

    Added sniffer class

--- nethound_module/main2.py
+++ nethound_module/main2.py
@@ -2,12 +2,13 @@ from monitors.dhcp_monitor import DHCPMonitor
 from safe_print import SafePrint
 from packets_queue import PacketsQueue
 import sys
-
+from sniffer import Sniffer
 
 if __name__ == '__main__':
     interface = sys.argv[1]
     packets = PacketsQueue()
     safe_print = SafePrint()
+    sniffer = Sniffer(interface, packets)
     dhcp = DHCPMonitor(interface, packets, safe_print)
     dhcp.start()
     dhcp.join()
---
+++ nethound_module/sniffer.py
@@ -0,0 +1,16 @@
+from threading import Thread
+from packets_queue import PacketsQueue
+import pyshark
+from time import sleep
+
+
+class Sniffer(Thread):
+    def __init__(self, interface: str, packets: PacketsQueue):
+        super(Sniffer, self).__init__()
+        self.packets = packets
+        self.interface = interface
+
+    def run(self) -> None:
+        capture = pyshark.LiveCapture(interface=self.interface)
+        for packet in capture.sniff_continuously():
+            self.packets.put(packet)
